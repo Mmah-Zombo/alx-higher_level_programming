18afe27743fde2d094a0ab70cb7fb284f218f50a
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-02 18:54:43 -0800

    task11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-# Write the string to the standard error stream
 sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19,\n")
-# Exit with a non-zero status code
 sys.exit(1)
