eb80385f284dde1cbf3cd76ee1730504f1ce41cb
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-03 16:47:53 -0800

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -6,4 +6,4 @@ for i in range(10):
             if (i, j) == (8, 9):
                 print(f"{i:01d}{j:01d}")
             else:
-                print(f"{i:01d}{j:01d}, ", end="")
+                print("{:01d}{:01d}, ".format(i, j), end="")
