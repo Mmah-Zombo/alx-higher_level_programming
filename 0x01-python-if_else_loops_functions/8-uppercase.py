31c7e406228432e0a2c79edcbe8f986e5a8a7255
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-04 17:04:54 +0000

    task8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(string):
     result = ""
     for ch in string:
         if ord(ch) >= 97 and ord(ch) <= 122:
-            result += chr(ord(ch) - 32)
             print(result)
         else:
             result += ch
