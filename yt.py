55de0c5561b82b01dc4578235c300554fae92cf6
M'mah Zombo <www.mariamzombo@gmail.com>
2023-01-05 22:32:04 +0000

    task2

---
+++ yt.py
@@ -0,0 +1,4 @@
+                                                                                                          
+str = "Holberton School"                                                                                                     
+print(3 * str)                                                                                                        
+print(str[:9])                                                                                                        
