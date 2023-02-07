b59e28af9d785fbfcbc5494e812d8181dc8a113b
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-07 02:50:29 +0000

    task0-3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-
 '''Tests if an object is exactly an instance
 of a specified class or an instance of the an inherited
-class
-
-Args: 
-    obj (object): the object to be tested
-    a_class (class): the specified class 
+class 
 '''
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     '''Checks if its an instance'''
     if isinstance(obj, a_class):
         return True
