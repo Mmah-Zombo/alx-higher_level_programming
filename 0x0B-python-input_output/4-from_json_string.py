e2f3a92ddac9e8766f599dd69fda32bcaeb7edad
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-08 02:31:29 +0000

    task0

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,5 +2,8 @@
 '''a function that returns an object (Python data structure) 
 represented by a JSON string
 '''
+import json
+
 def from_json_string(my_str):
-    with open()
+    '''Returns an object represented by a JSON string'''
+    return json.loads(my_str)
