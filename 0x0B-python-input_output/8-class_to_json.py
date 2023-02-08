ab00ea32081db3f7cbcc11fc77b7ca685cc8ae9d
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-08 03:33:19 +0000

    task8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module defines a Python class-to-JSON function"""
+
+def class_to_json(obj):
+        """Returns the dictionary representation of a simple data structure"""
+        return obj.__dict__
