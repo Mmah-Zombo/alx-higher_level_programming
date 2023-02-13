c4fa724860f315263f2f630ab98186dbdc5bae4d
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-13 00:56:23 +0000

    task1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,5 +8,5 @@ class Base:
         if id != None:
             self.id = id
         else:
-            __nb_objects += 1
-            id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
