7af5566588ccfd0452c9713367331bda118b8392
M'mah Zombo <www.mariamzombo@gmail.com>
2023-02-17 16:11:25 +0000

    fixed rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,11 +5,11 @@ from models.base import Base
 class Rectangle(Base):
     """Initialization of the class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
     def width(self):
