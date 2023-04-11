5a173aaa4801247783f680544174ada728f8cca5
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-11 18:49:50 +0000

    task16.1

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-function incrementAndCall(number, theFunction) {
-  const incrementedNumber = number + 1;
-  theFunction(incrementedNumber);
-}
-
-module.exports = {
-  incrementAndCall
+exports.addMeMaybe = function (number, theFunction) {
+  theFunction(++number);
 };
