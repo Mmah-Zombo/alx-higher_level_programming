107e42731c6656b08dd5ed93bd105eb36dbf9001
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-17 13:45:24 +0100

    task16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- Lists all records of the table second_table of the database hbtn_0c_0
 -- in MySQL server.
-SELECT `score`, `name` FROM `second_table` WHERE `name` != NULL
+SELECT `score`, `name` FROM `second_table` WHERE `name` != ""
 ORDER BY `score` DESC;
