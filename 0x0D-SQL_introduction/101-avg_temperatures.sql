f68bd4d4ec657c54b8849a11ae21797c45c976ad
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-17 14:15:32 +0100

    task18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,3 @@
--- script that displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp FROM temperatures
-WHERE month BETWEEN 7 AND 8
-GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+GROUP BY city ORDER BY avg_temp DESC;
