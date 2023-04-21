79684b2d84ed2333ca019eb9dbb9807b05c77591
M'mah Zombo <www.mariamzombo@gmail.com>
2023-04-21 17:07:11 +0100

    task0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
-import pymysql
+#!/usr/bin/python3
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) == 4:
-        conn = pymysql.connect(
+        conn = MySQLdb.connect(
             host="localhost",
             user=sys.argv[1],
-            password=sys.argv[2],
-            database=sys.argv[3],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
             port=3306,
-            )
+        )
         cur = conn.cursor()
         cur.execute("SELECT * FROM states ORDER BY id ASC")
         rows = cur.fetchall()
@@ -20,4 +20,3 @@ if __name__ == '__main__':
         conn.close()
     else:
         print("Usage: ./list_states.py <mysql_username> <mysql_password> <database_name>")
-
