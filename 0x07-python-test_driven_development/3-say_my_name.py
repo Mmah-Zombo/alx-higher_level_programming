2e1c145eb4ba322de3df19582c717a5f2d631d79
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-20 02:53:11 +0000

    task2

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""contains a function that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """the function"""
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    # Check if last_name is a string
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    # Print the name
+    full_name = first_name + " " + last_name
+    print("My name is", full_name.strip())
