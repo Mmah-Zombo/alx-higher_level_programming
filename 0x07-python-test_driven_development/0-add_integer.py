c3bac3b97ef24466d5d9c573516016f44b6cb10e
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-20 01:51:48 +0000

    task0 more tests

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """A module with an add_integer function"""
 
-
 def add_integer(a, b=98):
     """A function that adds 2 integer"""
     if not isinstance(a, int) and not isinstance(a, float):
